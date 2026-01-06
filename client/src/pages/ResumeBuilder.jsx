74cfedc3cef63dbde3a1bfe3ae441f15c2612639
Uzair <programmeruzair@gmail.com>
2026-01-06 17:57:39 +0530

    feat: Add resume loading functionality in ResumeBuilder component

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { dummyResumeData } from '../assets/assets';
 
 const ResumeBuilder = () => {
+
+  const { resumeId } = useParams();
+
+
+  const [resumeData, setResumeData] = React.useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    expirience: [],
+    education: [],
+    skills: [],
+    project: [],
+    template: "",
+    accent_color: "",
+    public: false
+  })
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((res) => res._id === resumeId);
+    if(resume){
+      setResumeData(resume);
+    }
+  }
+
+  useEffect(() => {
+    loadExistingResume();
+  },[])
   return (
     <div>
       <h1>Resume Builder page</h1>
